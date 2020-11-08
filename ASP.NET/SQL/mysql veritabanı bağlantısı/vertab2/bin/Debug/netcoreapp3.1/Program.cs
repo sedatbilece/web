7310b1fc84ac48d5281834da2d94f984e74701a8
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2020-11-08 18:55:16 +0300

    önemli bunlar

---
+++ ASP.NET/SQL/mysql veritabanı bağlantısı/vertab2/bin/Debug/netcoreapp3.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+
+namespace vertab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           GetMySqlConnection();
+        }
+
+
+
+
+    
+///////////////////////////////////////////////////////////////////////////
+
+        static void GetMySqlConnection(){
+
+             string connectstring=@"server=127.0.0.1;port=3306; database=mydb; user=root;password=123456;" ;
+
+    using(var connection =new  MySqlConnection(connectstring) ){
+
+        try{
+connection.Open();
+Console.WriteLine("baglantı pozitif");
+    
+        }catch(Exception e){
+
+            Console.WriteLine(e.Message);
+
+        }
+        finally{
+    connection.Close();
+}
+
+    }//using end
+
+        }//getsql end
+
+
+        }
+
+
+    }
+
